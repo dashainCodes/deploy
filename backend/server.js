461e5a3ac5e54bc2c46a1a0d74c9e8233099e0e2
dashainCodes <dashain2.07@gmail.com>
2024-03-19 05:38:18 +0545

    hi

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const app=express()
+const cors=require('cors');
+
+app(cors());
+
+app.all("*", (req, res, next) => {
+    
+    res.send("hiiiii its working.")
+  });
+
+app.listen(4001,()=>{
+    console.log("listening to 4001")
+})
